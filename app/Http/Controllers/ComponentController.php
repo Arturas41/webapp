5b6d29d0cdec4cbd41c6fb6b3c9f5c32db18f57b
vagrant <vagrant@homestead>
2019-12-30 00:08:09 +0000

    components_model

--- app/Http/Controllers/ComponentController.php
+++ app/Http/Controllers/ComponentController.php
@@ -3,6 +3,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Component;
+use App\User;
+
 class ComponentController  extends Controller
 {
 
@@ -34,23 +37,8 @@ class ComponentController  extends Controller
      */
     public function index()
     {
-        $data['brand'] = 'Brand New Website';
-        $data['navsearch'] = 'Search Now!';
+        $components = Component::all();
 
-        $data['components'][0]['name'] = 'Learning archive component';
-        $data['components'][1]['name'] = 'Random component';
-        $data['components'][2]['name'] = 'Random component2';
-
-        $data['components'][0]['description'] = 'Learning archive component description';
-        $data['components'][1]['description'] = 'Random component description';
-        $data['components'][2]['description'] = 'Random component2 description';
-
-        $data['components'][0]['href'] = '/learning';
-        $data['components'][1]['href'] = '#';
-        $data['components'][2]['href'] = '#';
-
-        $data['footer'] = 'Trademark, Copyright, and all that Jazz';
-
-        return view('components')->with($data);
+        return view('components', $components)->with('components', $components);;
     }
 }
--- resources/views/components.blade.php
+++ resources/views/components.blade.php
@@ -4,11 +4,11 @@
 
     @foreach($components as $component)
         <div class="jumbotron">
-            <h1>{{$component['name']}}</h1>
+            <h1>{{$component['title']}}</h1>
 
             <p>{{$component['description']}}</p>
 
-            <p><a class="btn btn-primary btn-lg" href="{{$component['href']}}" role="button">Click to enter</a></p>
+            <p><a class="btn btn-primary btn-lg" href="{{$component['url']}}" role="button">Click to enter</a></p>
         </div>
     @endforeach
 
