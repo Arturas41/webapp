33d0bd3a74bf7fb031d4f7d9d84305af1421744b
vagrant <vagrant@homestead>
2020-01-05 13:48:38 +0000

    scheduling_events_in_laravel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->exec('cd ~/code/webapp && ls')
+            ->everyMinute()
+            ->sendOutputTo('/home/vagrant/code/webapp/listing.txt');
     }
 
     /**
