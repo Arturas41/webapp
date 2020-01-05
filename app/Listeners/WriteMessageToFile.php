3cfbb344544b7f46a8605b48836e46945a9df4cd
vagrant <vagrant@homestead>
2020-01-05 14:27:59 +0000

    events_lisseners

---
+++ app/Listeners/WriteMessageToFile.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserLoggedIn;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Storage;
+
+class WriteMessageToFile
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  UserLoggedIn  $event
+     * @return void
+     */
+    public function handle(UserLoggedIn $event)
+    {
+        $message = $event->request->user()->name . ' just logged in to the application.';
+        Storage::append('loginactivity.txt', $message);
+    }
+}
