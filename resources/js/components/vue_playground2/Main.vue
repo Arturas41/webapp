b5e7f70fac8b939498ebc48c806b1a13b5ddddf1
vagrant <vagrant@homestead>
2020-01-16 23:28:54 +0000

    vue_dynamic_components

--- resources/js/components/vue_playground2/Main.vue
+++ resources/js/components/vue_playground2/Main.vue
@@ -6,7 +6,18 @@
         <vp2-slots-example>
             <h1 slot="title">{{ slotMarkup }}</h1>
             <h5 slot="subtitle">The Sub Title Passed In</h5>
+            <h1>slot</h1>
         </vp2-slots-example>
+        <div class="d-flex justify-content-center">
+            <div @click="selectedComponent = 'One'" class="p-2 m-2 btn btn-primary">One</div>
+            <div @click="selectedComponent = 'Two'" class="p-2 m-2 btn btn-primary">Two</div>
+            <div @click="selectedComponent = 'Three'" class="p-2 m-2 btn btn-primary">Three</div>
+        </div>
+        <div class="d-flex justify-content-center">
+            <keep-alive>
+                <component :is="selectedComponent"></component>
+            </keep-alive>
+        </div>
     </div>
 </template>
 
@@ -14,13 +25,17 @@
 
     import BootstrapCard from '../vue_playground2/BootstrapCard.vue';
     import SlotsExample from '../vue_playground2/SlotsExample.vue';
+    import One from '../vue_playground2/OneComponent.vue';
+    import Two from '../vue_playground2/TwoComponent.vue';
+    import Three from '../vue_playground2/ThreeComponent.vue';
 
     export default {
-        components: {BootstrapCard, SlotsExample},
+        components: {BootstrapCard, SlotsExample, One, Two, Three},
 
         data(){
             return{
-                slotMarkup: 'THE REAL DEAL!'
+                slotMarkup: 'THE REAL DEAL!',
+                selectedComponent: 'One' 
             }
         }
     }
