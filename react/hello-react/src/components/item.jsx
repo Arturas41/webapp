9820ec49cc12af7f2630f5604cb3435d362d2298
vagrant <vagrant@homestead>
2020-03-09 01:01:36 +0000

    react_events

--- react/hello-react/src/components/item.jsx
+++ react/hello-react/src/components/item.jsx
@@ -2,30 +2,34 @@ import React, { Component } from "react";
  
 class Item extends Component {
   state = {
-    listitems: []
+    count: 5
   };
  
-    render() {
-      return <React.Fragment>{this.renderItems()}</React.Fragment>;
-    }
-  renderItems() {
-    if (this.state.listitems.length === 0) {
-      return (
-        <div class="alert alert-warning" role="alert">
-          There are no items to list!
-        </div>
-      );
-    } else {
-      return (
-        <ul className="list-group">
-          {this.state.listitems.map(listitem => (
-            <li className="list-group-item list-group-item-primary">
-              {listitem}
-            </li>
-          ))}
-        </ul>
-      );
-    }
+  handleIncrement = (e) => {
+    this.setState({ count: this.state.count + 1 });
+    alert(e.message)
+  };
+ 
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.styleBadge()}>{this.styleCount()}</span>
+        <button onClick={() => {this.handleIncrement({ message: "Its Friday!" });}} className="btn btn-primary">
+          Increment
+        </button>
+      </React.Fragment>
+    );
+  }
+ 
+  styleBadge() {
+    let classes = "badge m-3 badge-";
+    classes += this.state.count === 0 ? "warning" : "info";
+    return classes;
+  }
+ 
+  styleCount() {
+    const { count } = this.state;
+    return count === 0 ? "No Items" : count;
   }
 }
  
