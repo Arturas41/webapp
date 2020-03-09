1326dbb8321fd426fcb887c8fbd4908bb68f55f6
vagrant <vagrant@homestead>
2020-03-09 00:39:52 +0000

    more_react

---
+++ react/hello-react/src_copy/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h2>to Babel compiler</h2>;
+console.log(element);
+ReactDOM.render(element, document.getElementById("root"));
