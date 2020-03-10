f1644a29382ee5b6496ddf2a803a4a8182c2265d
vagrant <vagrant@homestead>
2020-03-10 04:42:09 +0000

    .

--- node/react/hello-react/src/components/item.jsx
+++ node/react/hello-react/src/components/item.jsx
@@ -5,15 +5,22 @@ class Items extends Component {
   state = {
     items: [{ id: 1, value: 0 }, { id: 2, value: 10 }, { id: 3, value: 0 }]
   };
+ 
+  handleDelete = itemId => {
+    const items = this.state.items.filter(item => item.id !== itemId);
+    this.setState({ items: items });
+  };
+ 
   render() {
     return (
       <React.Fragment>
         {this.state.items.map(item => (
-          <Item key={item.id} value={item.value}>
-            <div className="card-footer text-muted">
-              Footer For Item Number <b className="badge badge-info">{item.id}</b>
-            </div>
-          </Item>
+          <Item
+            key={item.id}
+            value={item.value}
+            onDelete={this.handleDelete}
+            id={item.id}
+          />
         ))}
       </React.Fragment>
     );
---
+++ node/react/hello-react/src/components/items.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import Item from "./item";
+ 
+class Items extends Component {
+  state = {
+    items: [{ id: 1, value: 0 }, { id: 2, value: 10 }, { id: 3, value: 0 }]
+  };
+ 
+  handleDelete = itemId => {
+    const items = this.state.items.filter(item => item.id !== itemId);
+    this.setState({ items: items });
+  };
+ 
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.items.map(item => (
+          <Item
+            key={item.id}
+            value={item.value}
+            onDelete={this.handleDelete}
+            id={item.id}
+          />
+        ))}
+      </React.Fragment>
+    );
+  }
+}
+ 
+export default Items;
