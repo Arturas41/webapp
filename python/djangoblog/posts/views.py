5042d6640c232641117e600254caebd49487ac66
vagrant <vagrant@homestead>
2020-03-12 17:40:16 +0000

    django_slug_based_routing

--- python/djangoblog/posts/views.py
+++ python/djangoblog/posts/views.py
@@ -4,3 +4,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.all().order_by('date')
     return render(request, 'posts/post_list.html', {"posts": posts})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/post_detail.html', {'post': post})
